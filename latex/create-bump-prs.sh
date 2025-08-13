84271bd2567838ff90f150b5a43e185e80a3428c
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-13 18:27:44 +0200

    ci(bump): fix create-bump-prs.sh prompt

--- latex/create-bump-prs.sh
+++ latex/create-bump-prs.sh
@@ -17,7 +17,7 @@ if [ -f .bump.csv.bak ]; then
 		\perl -i -p -e "s|\Q${l1}\E|${l2}|g" Dockerfile
 		\git add Dockerfile
 		\git commit -S -m "build(deps): bump ${p} from ${v1} to ${v2}"
-		read -n 1 -s -r -p "Please review and push commit in branch \"${b}\". Press enter to continue..."
+		read -r -p "Please review and push commit in branch \"${b}\". Press enter to continue..."
 		\gh pr create -f -l build -l dependencies
 		\git checkout -
 	done <<<"${csv}"
