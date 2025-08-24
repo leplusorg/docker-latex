20d7dcf851cbb757642104805d2f476654a428e8
Thomas Leplus <thomasleplus@users.noreply.github.com>
2025-08-24 10:40:53 +0200

    ci(bump): override entry point

--- latex/bump-apk-versions.sh
+++ latex/bump-apk-versions.sh
@@ -31,7 +31,7 @@ if [ -f /.dockerenv ]; then
 
 else
 
-	\docker run --pull always --rm -t --user root -v "$(pwd):/opt/bump" "leplusorg/${PWD##*/}:alpine" /opt/bump/"$(\basename "${0}")"
+	\docker run --pull always --rm -t --user root -v "$(pwd):/opt/bump" --entrypoint /opt/bump/"$(\basename "${0}")" "leplusorg/${PWD##*/}:alpine"
 
 	./create-bump-prs.sh
 
